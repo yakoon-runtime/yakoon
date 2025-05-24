15d80608b50d4c2e7f987c62ece2c3bca54211cc
Stefan Bergmann <stefan.bergmann@enterstry.com>
2025-05-24 21:45:56 +0200

    feat(core): add CmdStatus with version info, uptime and UTC timestamp

--- yakoon/platform/commands/system/cmdset.py
+++ yakoon/platform/commands/system/cmdset.py
@@ -1,9 +1,9 @@
 from typing import Sequence, Type
 from yakoon.engine.core.command import Command
 from yakoon.engine.core.commandset import CommandSet
-
 from yakoon.platform.commands.system.cmd_switch import CmdSwitch
 from yakoon.platform.commands.system.cmd_help import CmdHelpSystem
+from yakoon.platform.commands.system.cmd_version import CmdVersion
 from yakoon.platform.settings import Settings
 
 
@@ -14,6 +14,7 @@ class PlatformSystemCommands(CommandSet):
     @classmethod
     def commands(cls) -> Sequence[Type[Command]]: 
         return [
+            CmdVersion,
             CmdHelpSystem,
             CmdSwitch,
         ]
