3c0b4f31ff6d129a44d677f1c121779cd9ca2cbd
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-02-05 13:36:30 +0100

    refactor(Project): extrakt console app in seperate package.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,6 @@
 cd ..
-pip install -e packages/yakoon-core-base -e packages/yakoon-core-platform -e packages/yakoon-core-compose -e packages/yakoon-core-hosts 
+pip install \
+    -e packages/yakoon-core-base        \
+    -e packages/yakoon-core-platform    \
+    -e packages/yakoon-core-compose     \
+    -e packages/yakoon-host-console     \
--- scripts/install_plugins.sh
+++ scripts/install_plugins.sh
@@ -1,2 +1,6 @@
 cd ..
-pip install -e plugins/yakoon-shell -e plugins/yakoon-office -e plugins/yakoon-auth
+pip install \
+    -e plugins/yakoon-shell         \
+    -e plugins/yakoon-office        \ 
+    -e plugins/yakoon-auth          \
+    
