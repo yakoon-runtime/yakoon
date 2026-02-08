d0f96e392f1e9d67deb9cbfc4f83e7d845bf4f6e
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-02-08 23:01:13 +0100

    feat(UIStateProvider): show prompt prefix in prompt.

--- packages/yakoon-host-kivy/src/yakoon/kivy/states/state_provider.py
+++ packages/yakoon-host-kivy/src/yakoon/kivy/states/state_provider.py
@@ -1,6 +1,9 @@
 
 from dataclasses import dataclass
 
+from yakoon.base.runtime.session.session import Session
+from yakoon.base.utils.format import format_prompt
+
 @dataclass(frozen=True)
 class UIState:
     prompt_prefix: str
@@ -9,10 +12,10 @@ class UIState:
 
 class UIStateProvider:
 
-    def __init__(self, session):
+    def __init__(self, session: Session):
         self.session = session
 
     def __call__(self) -> UIState:  
-        prompt = getattr(self.session, "prompt_prefix", "stefan@app$")
+        prompt = format_prompt(self.session)
         secret = getattr(self.session, "prompt_secret", False)
         return UIState(prompt_prefix=prompt, prompt_secret=secret)
