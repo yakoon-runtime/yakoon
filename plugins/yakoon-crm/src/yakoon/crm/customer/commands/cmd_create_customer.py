909c11058c9c703a6fc6652f563e0845ab6fc58e
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-02-10 20:40:15 +0100

    feat(Command): add a new wfcommand.

--- plugins/yakoon-crm/src/yakoon/crm/customer/commands/cmd_create_customer.py
+++ plugins/yakoon-crm/src/yakoon/crm/customer/commands/cmd_create_customer.py
@@ -11,19 +11,33 @@ class CmdCreateCustomer(Command):
     key = "crm.customer.create"
 
     async def run(self, session: Session, request: Request):
-
         audits = self.services.get(AuditLogService)
-        presenter = await self.get_presenter(session)
+        wf = self.services.get(ports. WorkflowService)
 
         first = request.option("first")
         last = request.option("last")
+        mail_opt_in = request.option("mail_opt_in")
+        mail = request.option("mail")
+
         if not first or not last:
-            raise ValueError("Missing required parameters: --first, --last")
+            raise ValueError("Missing required: --first, --last")
+
+        # Optional: Konsistenz
+        if mail_opt_in in {"yes", "true", "1"} and not mail:
+            raise ValueError("mail_opt_in=yes requires --mail")
 
         customer_id = f"CUST-{uuid4().hex[:8]}"
 
-        await audits.audit(f"Create Customer: {first} {last} (id={customer_id})")
-        await presenter.emit("created")
+        # “Ernte”: audit + event
+        await audits.audit(f"Customer created: {first} {last} id={customer_id}")
 
-        wf = self.services.get(ports.WorkflowService)
-        wf.set_value(session, request.batch_id, "customer.id", customer_id)
+        # TODO: Jetzt im Workflow weiter.... 
+
+        # Ergebnis zurück in Workflow-Context
+        #batch_id = request.arg(0)   # falls ihr batch_id/step_id als args übergebt
+        #step_id  = request.arg(1)
+
+        #wf.set_value(session, batch_id, "customer.id", customer_id)
+
+        # Workflow weiterlaufen lassen
+        #wf.advance_after_run(session, batch_id=batch_id, step_id=step_id)
