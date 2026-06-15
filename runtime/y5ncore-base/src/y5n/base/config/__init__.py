b8efc79aa72ffaa5dc95d8d148203cb18e84b794
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-06-15 21:55:44 +0200

    chore: remove unused y5n.base.config module (all configs are now app-local)

--- runtime/y5ncore-base/src/y5n/base/config/__init__.py
+++
@@ -1,9 +0,0 @@
-from .loader import load_config, save_config
-from .model import RuntimeConfig, YakoonConfig
-
-__all__ = [
-    "load_config",
-    "RuntimeConfig",
-    "save_config",
-    "YakoonConfig",
-]
