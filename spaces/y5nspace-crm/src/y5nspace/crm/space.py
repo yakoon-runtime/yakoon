1e02c83228446ea9dac0916719d62f704c4ad2d0
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-06-27 11:00:27 +0200

    fix: move templates from en/ to de/, translate to German

--- spaces/y5nspace-crm/src/y5nspace/crm/space.py
+++ spaces/y5nspace-crm/src/y5nspace/crm/space.py
@@ -1,6 +1,13 @@
 from y5n.api.nodes import Node
 
-from .runtime import contact_add, contact_delete, contact_edit, contact_list, contact_show, setup
+from .runtime import (
+    contact_add,
+    contact_delete,
+    contact_edit,
+    contact_list,
+    contact_show,
+    setup,
+)
 
 crm = Node(
     key="crm",
@@ -10,28 +17,43 @@ crm = Node(
     setup=setup,
 )
 
-crm.add(Node(
-    key="contact-list",
-    name="Contact list",
-    run=contact_list.run,
-))
-crm.add(Node(
-    key="contact-add",
-    name="Contact add",
-    run=contact_add.run,
-))
-crm.add(Node(
-    key="contact-show",
-    name="Contact show",
-    run=contact_show.run,
-))
-crm.add(Node(
-    key="contact-edit",
-    name="Contact edit",
-    run=contact_edit.run,
-))
-crm.add(Node(
-    key="contact-delete",
-    name="Contact delete",
-    run=contact_delete.run,
-))
+crm.add(
+    Node(
+        key="contact-list",
+        name="Contact list",
+        run=contact_list.run,
+        anonymous=True,
+    )
+)
+crm.add(
+    Node(
+        key="contact-add",
+        name="Contact add",
+        run=contact_add.run,
+        anonymous=True,
+    )
+)
+crm.add(
+    Node(
+        key="contact-show",
+        name="Contact show",
+        run=contact_show.run,
+        anonymous=True,
+    )
+)
+crm.add(
+    Node(
+        key="contact-edit",
+        name="Contact edit",
+        run=contact_edit.run,
+        anonymous=True,
+    )
+)
+crm.add(
+    Node(
+        key="contact-delete",
+        name="Contact delete",
+        run=contact_delete.run,
+        anonymous=True,
+    )
+)
