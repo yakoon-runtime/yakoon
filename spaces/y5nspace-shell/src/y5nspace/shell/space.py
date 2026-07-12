ce9d21a7d4f5bddb4e7dcf1c297262cff5265d38
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-07-12 17:58:08 +0200

    refactor(experiment): remove migrated commands from old shell space

--- spaces/y5nspace-shell/src/y5nspace/shell/space.py
+++ spaces/y5nspace-shell/src/y5nspace/shell/space.py
@@ -1,10 +1,7 @@
 from y5n.api.nodes import Node
 
 from .runtime.ident.space import ident
-from .runtime.jobs.space import jobs
 from .runtime.labs.space import labs
-from .runtime.net.space import net
-from .runtime.session.space import session
 from .runtime.setup import setup
 
 # ----------------------------------
@@ -25,8 +22,5 @@ shell = Node(
 # MOUNT
 # ----------------------------------
 
-shell.mount(jobs)
 shell.mount(labs)
-shell.mount(net)
-shell.mount(session)
 shell.mount(ident)
