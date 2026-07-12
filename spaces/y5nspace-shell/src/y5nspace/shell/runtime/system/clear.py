16ec7cdb255acd95bf6670e0a08638aef32f7b31
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-07-12 13:53:50 +0200

    refactor(experiment): remove migrated commands from old shell space
    Delete system commands (cd, ls, pwd, clear, set, man, info, welcome)
    and their templates from y5nspace-shell. All are now native bundles
    under bundles/root/.

--- spaces/y5nspace-shell/src/y5nspace/shell/runtime/system/clear.py
+++
@@ -1,6 +0,0 @@
-from y5n.api.dsl import view
-from y5n.api.nodes import NodeSpace
-
-
-async def run(space: NodeSpace):
-    yield view(clear=True)
--- spaces/y5nspace-shell/src/y5nspace/shell/space.py
+++ spaces/y5nspace-shell/src/y5nspace/shell/space.py
@@ -1,14 +1,11 @@
 from y5n.api.nodes import Node
 
 from .runtime.ident.space import ident
-from .runtime.info import run as info
 from .runtime.jobs.space import jobs
 from .runtime.labs.space import labs
 from .runtime.net.space import net
 from .runtime.session.space import session
 from .runtime.setup import setup
-from .runtime.system.space import system
-from .runtime.welcome import run as welcome_run
 
 # ----------------------------------
 # SHELL
@@ -17,39 +14,10 @@ from .runtime.welcome import run as welcome_run
 shell = Node(
     key="shell",
     name="Shell",
-
     anonymous=True,
     resolvable=False,
     navigable=True,
     setup=setup,
-)
-
-# ----------------------------------
-# STATUS
-# ----------------------------------
-
-shell.add(
-    Node(
-        key="info",
-        run=info,
-        anonymous=True,
-        resolvable=True,
-        navigable=False,
-    )
-)
-
-# ----------------------------------
-# WELCOME
-# ----------------------------------
-
-shell.add(
-    Node(
-        key="welcome",
-        run=welcome_run,
-        anonymous=True,
-        resolvable=True,
-        navigable=False,
-    )
 )
 
 
@@ -57,7 +25,6 @@ shell.add(
 # MOUNT
 # ----------------------------------
 
-shell.mount(system)
 shell.mount(jobs)
 shell.mount(labs)
 shell.mount(net)
