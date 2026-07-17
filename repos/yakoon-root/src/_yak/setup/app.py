176b11a4f7fde824a8e726d7ba5489841a8c6db8
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-07-17 17:29:47 +0200

    fix(root): set expose: false, remove obsolete _yak/setup/

--- repos/yakoon-root/src/_yak/setup/app.py
+++
@@ -1,9 +0,0 @@
-from .ports import GREET
-from y5n.api.nodes import NodeSpace
-
-
-async def run(space: NodeSpace):
-    def greet(first_name: str, last_name: str) -> str:
-        return f"Hello {first_name} {last_name} from root context!"
-
-    space.ports.provide(GREET, greet)
