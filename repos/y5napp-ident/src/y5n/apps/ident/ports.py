dd869fa6943dd7735edca00a7e8bf2aa66a17580
Stefan Bergmann <stefan.bergmann.cx@gmail.com>
2026-07-22 14:53:17 +0200

    chore(ident): remove unused ports.py

--- repos/y5napp-ident/src/y5n/apps/ident/ports.py
+++
@@ -1,7 +0,0 @@
-from y5n.runtime.api.ports import Port
-
-NAMESPACES = Port("ident.namespaces")
-USER_SERVICE = Port("ident.user.service")
-GROUP_SERVICE = Port("ident.group.service")
-JOIN_SERVICE = Port("ident.join.service")
-PERMGRANT_SERVICE = Port("ident.permgrant.service")
